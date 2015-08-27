b7a73f15831f6b1c9e131bcd4c9a62fce701410b
ShradhaV <shradha.vijay@micromaxinfo.com>
2015-08-27 15:07:30 +0530

    Adding unified code for TabletInfo and SalesTrack

---
+++ SalesTrack/src/com/micromax/salestrack/Setting.java
@@ -0,0 +1,15 @@
+
+package com.micromax.salestrack;
+
+public class Setting {
+
+	public static final boolean IS_TABLET = true;
+	public static final boolean IS_RUSSIA = false;
+	public static final boolean IS_LOLLIPOP = false;
+	public static final boolean IS_WINGTECH = false;
+	public static final boolean IS_TOPWEIZ = false;
+	public static final boolean IS_SINGLESIM = true;
+	public static final String TAC_ID_TABLET = "42";
+	public static final boolean IS_WIFIONLY_TABLET = true;
+	
+}
